9799d793835b000d98008c3fc8910748ca27a999
datasave582-design <datasave582@gmail.com>
2026-09-24 13:50:19 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const V='mg11';
+const V='mg13';
 self.addEventListener('install',e=>{e.waitUntil(caches.open(V).then(c=>c.addAll(['./','index.html','fb.js','manifest.json','icon-192.png'])));self.skipWaiting()});
 self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(k=>Promise.all(k.filter(x=>x!=V).map(x=>caches.delete(x))))));
 self.addEventListener('fetch',e=>{if(e.request.method!='GET'||new URL(e.request.url).pathname.startsWith('/__/')||!e.request.url.startsWith(self.location.origin))return;e.respondWith(fetch(e.request).then(r=>{const c=r.clone();caches.open(V).then(x=>x.put(e.request,c));return r}).catch(()=>caches.match(e.request)))});
